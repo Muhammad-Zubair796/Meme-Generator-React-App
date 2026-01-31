8a42a7d8dbe42fc845573dddbad1bcf15ddee2f7
Muhammad-Zubair796 <adrainbialon@gmail.com>
2026-01-31 13:10:38 +0500

    Complete Meme Generator project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import trollFace from '../assets/troll-face.png'
+
+export default function Header() {
+    return(
+        <header className="header">
+            <img src={trollFace} alt="Troll Face" 
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,62 @@
+import React, { use, useEffect } from "react";
+import { useState } from "react";
+
+export default function Main() {
+    const [meme, setMeme] = React.useState({
+        topText: "one does not simply",
+        bottomText: "walk into mordor",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemes, setAllMemes] = useState([])
+    useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res=>res.json())
+        .then(data => setAllMemes(data.data.memes))
+    },[])
+
+    function handleChange(event) {
+        const {value,name}=event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    
+
+    }
+    function handleClick() {
+        const randomIndex = Math.floor(Math.random() * allMemes.length)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: allMemes[randomIndex].url
+        }))
+    }
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                    />
+                </label>
+                <button onClick={handleClick}>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
